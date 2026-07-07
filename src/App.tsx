7457b471c63ba9249c743446137b014c6c0eca7c
JulianAgPerez <juli.agustin.perez@gmail.com>
2026-07-07 01:42:30 -0300

    chore: remove @vercel/speed-insights dependency

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,6 @@ import { ThemeProvider } from "@mui/material";
 import { Provider } from "react-redux";
 import store from "./redux/Store";
 import "./styles/scrollbar.css";
-import { SpeedInsights } from "@vercel/speed-insights/react";
 import { lightTheme } from "./components/Themes/LightTheme";
 
 export const baseUrl = import.meta.env.VITE_API_URL;
@@ -13,7 +12,6 @@ export const App = () => {
   return (
     <ThemeProvider theme={lightTheme}>
       <Provider store={store}>
-        <SpeedInsights />
         <AppRouter />
       </Provider>
     </ThemeProvider>
